933337b7e39613906a8d32a53eb9c4ad3bc286d6
ScottGeek <scott_edwards@scottgeek.com>
2023-12-01 14:47:51 -0500

    Added simple project relationship

--- TimeTracker.Shared/Entities/TimeEntry.cs
+++ TimeTracker.Shared/Entities/TimeEntry.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace TimeTracker.Shared.Entities
 {
-   
-        public class TimeEntry
-        {
-            public int Id { get; set; }
-            public required string Project { get; set; }
-            public DateTime Start { get; set; } = DateTime.Now;
-            public DateTime? End { get; set; }
-            public DateTime DateCreated { get; set; } = DateTime.Now;
-            public DateTime? DateUpdated { get; set; }
 
-        }
+    public class TimeEntry : BaseEntity
+    {
+
+        public int? ProjectId { get; set; }
+        public Project? Project { get; set; }
+        public DateTime Start { get; set; } = DateTime.Now;
+        public DateTime? End { get; set; }
+
+
+    }
 
 }
